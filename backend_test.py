fee24c828a22c55acbefc565a5fa59ef10dae417
Awesi <122213595+Awesi@users.noreply.github.com>
2024-07-16 11:51:12 +0100

    almost works

---
+++ backend_test.py
@@ -0,0 +1,33 @@
+import boto3
+import json
+
+boto3.setup_default_session(profile_name='hangman-client')
+client = boto3.client('bedrock-runtime', region_name="")
+
+def main(prompt, message_history):
+
+    input_data = """{\"prompt\":\"\\n\\nHuman: You are playing a game of hangman, and you have picked
+    the word sunshine, use the history to determine and the current guess here: """ + prompt + """ to determine
+    which letters are revealed\\n\\nAssistant:\",
+    \"temperature\":0.5,
+    \"max_gen_len\":512,
+    \"top_p\":0.9}"""
+
+    model_id = 'meta.llama3-70b-instruct-v1:0'
+
+    response = client.invoke_model(contentType='application/json', body=input_data, modelId=model_id)
+    inference_result = response['body'].read().decode('utf-8')
+    output = json.loads(inference_result)
+    
+    return (output['generation'])
+
+    # user_message = f'''You are playing a game of hangman, and you have picked
+    # the word sunshine, use the history to determine and the current guess here: {prompt} to determine
+    # which letters are revealed'''
+
+
+    
+
+if __name__ == '__main__':
+    output = main()
+    print(output)
