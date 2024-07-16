ec4e2973f806907ff14d2c0885fcb760adf38bfa
Awesi <122213595+Awesi@users.noreply.github.com>
2024-07-16 17:18:45 +0100

    working game with random objects and working ai and 21 question and retry capabilities

--- backend_test.py
+++ backend_test.py
@@ -1,5 +1,7 @@
 import boto3
 import json
+from random import choice
+from objectlist import rand_word
 
 
 count = 0
@@ -14,27 +16,38 @@ def main(prompt, message_history):
     global word
     model_id = 'meta.llama3-70b-instruct-v1:0'
 
-    if count == 0:
-        data2 = {
-        "prompt": f"\n\nHuman: Pick a random object that you are confident you know a lot about. To do this, pick a random number from 1 to 10. If 1, pick an object; If 2, pick a food; If 3, pick an animal... . Return a one word answer that is just that word. DO NOT respond with more than a one word answer in quotation marks\n\nAssistant:",
-        "temperature": 0.5,
-        "max_gen_len": 512,
-        "top_p": 0.9
-        }
-
-        input_data2 = json.dumps(data2)
-
-        response2 = client.invoke_model(contentType='application/json', body=input_data2, modelId=model_id)
-        inference_result2 = response2['body'].read().decode('utf-8')
-        output2 = json.loads(inference_result2)
-        
-        word = (output2['generation'])
-
-    print(word)
+    current_prompt = ''
+    gameprompt = f"The user is playing a game of 21 questions with you. This has been the game so far {message_history}. If this number {count} is 21, the user has lost, state this, you are current on the {count}th questions, so display this, return to 0 if retry is entered by the user. the word the user is trying to guess is {word}. The user will ask you yes or no questions about the object which you must answer truthfully. The user is also allowed to guess the object. You MUST NOT reveal or say {word} unless the user makes an explicit guess about {word}. If this happens, congratulate the user as they have won. This is the user's first question " + prompt, 
+    checkprompt = f"Look at this word: {word}, if you think it is an umbrella term, return True, otherwise, return false. ONLY RETURN A SINGLE WORD, EITHER TRUE OR FALSE. DO NOT RETURN MORE THAN ONE WORD"
 
     data = {
-    "prompt": f"\n\nHuman: The user is playing a game of 21 questions with you. This has been the game so far {message_history}, use this to determine how many yes or no questions the user has asked, if it is 21, then they have lost and state this. the word the user is trying to guess is {word}. The user will ask you yes or no questions about the object which you must answer truthfully. The user is also allowed to guess the object. You MUST NOT reveal or say {word} unless the user makes an explicit guess about {word}. If this happens, congratulate the user as they have won. This is the user's first question " + prompt + " \n\nAssistant:",
-    "temperature": 0.5,
+    "prompt": f"\n\nHuman: {current_prompt}\n\nAssistant:",
+    "temperature": 0.9,
+    "max_gen_len": 512,
+    "top_p": 0.9
+    }
+
+    if count == 0:  
+        word = rand_word()
+
+    if prompt.lower() == 'retry':   
+        count = 0
+        print(count)
+        word = rand_word()
+
+    if count == 22:
+        count = 0
+        holdword = word
+        word = rand_word()
+        return (f'Unlucky, you did not get the word, the word was: {holdword}!')
+    
+    print(word)
+
+    current_prompt = gameprompt
+
+    data = {
+    "prompt": f"\n\nHuman: {current_prompt}\n\nAssistant:",
+    "temperature": 0.9,
     "max_gen_len": 512,
     "top_p": 0.9
     }
@@ -50,7 +63,10 @@ def main(prompt, message_history):
     
     count += 1
 
-    return (output['generation'])
+    print(count)
+
+    if prompt.lower != 'retry':
+        return (output['generation'])
 
     # user_message = f'''You are playing a game of hangman, and you have picked
     # the word sunshine, use the history to determine and the current guess here: {prompt} to determine
